5cda5d4ae91cf6f7f15ef3d539f8cd7f1bff4d30
MajaSveljo <majasveljo@gmail.com>
2021-09-01 14:47:45 +0200

    aded a redirect

--- src/ErrorBoundry.js
+++ src/ErrorBoundry.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundry extends Component {
   state = {
     hasError: false,
+    redirect: false,
   };
 
   static getDerivedStateFromError() {
@@ -13,14 +14,25 @@ class ErrorBoundry extends Component {
   componentDidCatch(error, info) {
     // would log this to Sentry or another monitoring services
     console.error("ErrorBoundry caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
+  // this is a bad place to do it because this method doesn't get called on first render
+  //   componentDidUpdate() {
+  //     if (this.state.hasError) {
+  //       setTimeout(() => this.setState({ redirect: true }), 5000);
+  //     }
+  //   }
+
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing has an error.
-          <Link to="/">Click here to go back to the homepage</Link>
+          <Link to="/">Click here to go back to the homepage</Link> or wait 5
+          seconds
         </h2>
       );
     }
